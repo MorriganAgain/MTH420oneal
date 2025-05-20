48086e134453c8f27487bdf7ad1aa0a1c4b9db23
MorriganAgain <onealf@oregonstate.edu>
2025-05-19 18:16:42 -0700

    nump_lab fix

--- NumpyIntro/numpy_intro.py
+++ NumpyIntro/numpy_intro.py
@@ -8,11 +8,11 @@
 import numpy as np
 
 
-def prob1():
+def prob1(): 
     """ Define the matrices A and B as arrays. Return the matrix product AB. """
     
     A = np.array([[3, -1, 4], 
-                 [1, 5, 9]])
+                 [1, 5, -9]])
     B = np.array([[2, 6, -5, 3], 
                  [5, -8, 9, 7],
                  [9, -3, -2, -3]])
